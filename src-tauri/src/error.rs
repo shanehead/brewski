2f6da93766f4ca6373ad773b335bb50726a4c88d
Shane Head <shane.head@gmail.com>
2026-05-04 20:57:14 -0400

    feat: add SeaORM error variant to AppError

--- src-tauri/src/error.rs
+++ src-tauri/src/error.rs
@@ -1,6 +1,8 @@
 #[derive(Debug, thiserror::Error)]
 pub enum AppError {
     #[error("database error: {0}")]
+    Database(#[from] sea_orm::DbErr),
+    #[error("database error: {0}")]
     Sqlx(#[from] sqlx::Error),
     #[error("not found")]
     NotFound,
