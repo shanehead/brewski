e8975f59c28d35ad1e6cef2a294037fa6545b466
Shane Head <shane.head@gmail.com>
2026-05-21 10:17:11 -0400

    feat: wrap ingredient tables in Fermentables, Hops, and Yeast cards

--- src/lib/components/tabs/IngredientsTab.svelte
+++ src/lib/components/tabs/IngredientsTab.svelte
@@ -1,14 +1,24 @@
+<!-- src/lib/components/tabs/IngredientsTab.svelte -->
 <script lang="ts">
   import type { Recipe } from "$lib/api";
   import FermentablesTable from "$lib/components/ingredients/FermentablesTable.svelte";
   import HopsTable from "$lib/components/ingredients/HopsTable.svelte";
   import YeastsTable from "$lib/components/ingredients/YeastsTable.svelte";
+  import Card from "$lib/components/Card.svelte";
 
   let { recipe, onchange }: { recipe: Recipe; onchange: () => void } = $props();
 </script>
 
-<div class="flex flex-col gap-8">
-  <FermentablesTable {recipe} {onchange} />
-  <HopsTable {recipe} {onchange} />
-  <YeastsTable {recipe} {onchange} />
+<div class="flex flex-col gap-4">
+  <Card title="Fermentables">
+    <FermentablesTable {recipe} {onchange} />
+  </Card>
+
+  <Card title="Hops">
+    <HopsTable {recipe} {onchange} />
+  </Card>
+
+  <Card title="Yeast">
+    <YeastsTable {recipe} {onchange} />
+  </Card>
 </div>
