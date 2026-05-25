27289d82616ae6fe19aba1442fca6c5dbe835a10
Shane Head <shane.head@gmail.com>
2026-05-24 23:57:25 -0400

    feat(routes): add /baseline-recipe/[id] route

---
+++ src/routes/baseline-recipe/[id]/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import type { PageData } from "./$types";
+  import BaselineRecipeView from "$platform/BaselineRecipeView.svelte";
+
+  let { data }: { data: PageData } = $props();
+</script>
+
+<BaselineRecipeView id={data.id} />
